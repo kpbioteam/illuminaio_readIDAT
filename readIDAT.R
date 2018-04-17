9410ef3a46e2a2fe6967743b11f1f3a6b7ca58ce
Ubuntu <ubuntu@ip-172-31-23-81.ec2.internal>
2018-04-17 14:03:51 +0000

    changes

--- readIDAT.R
+++ readIDAT.R
@@ -1,10 +1,15 @@
 require("illuminaio", quietly = TRUE)
+require("minfi", quietly = TRUE)
 
 args <- commandArgs(trailingOnly = TRUE)
 
-input = args[1]
-output = args[2]
+input1 = args[1]
+input2 = args[2]
+output = args[3]
 
-idat <- readIDAT(input)
+idat_red <- readIDAT(input1)
+idat_grn <- readIDAT(input2)
 
-save(idat, file = output)
+out <- RGChannelSet(Red = idat_red$Quants, Green = idat_grn$Quants)
+
+save(out, file = output)
